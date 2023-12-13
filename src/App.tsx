da24d7b46176c2f2cd79edf02f2790639a143043
Xobaib Mahmoodi <xobaib2001.m@gmail.com>
2023-12-13 11:06:10 +0330

    building ExpenseList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+import ExpenseList from "./components/ExpenseList";
+
 const App = () => {
-  return <div>Helo World</div>;
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "milk", amount: 5, category: "Groceries" },
+    { id: 2, description: "eggs", amount: 3, category: "Groceries" },
+    { id: 3, description: "movie", amount: 20, category: "Entertaiment" },
+    { id: 4, description: "lamp", amount: 4.33, category: "Utilities" },
+  ]);
+
+  const handleDelete = (id: number) => {
+    setExpenses(expenses.filter((expense) => expense.id !== id));
+  };
+
+  return (
+    <div>
+      <ExpenseList expenses={expenses} onDelete={handleDelete} />
+    </div>
+  );
 };
 
 export default App;
